acce46e811d7e965dc901ac605310f900a87edce
Mirco Kraenz <software@kraenz.eu>
2025-01-18 16:27:04 +0100

    feat(#1): almost done. only overworld-in-grass remaining

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,11 +1,23 @@
 import { fail, type Actions } from '@sveltejs/kit';
+import { readFileSync, writeFileSync } from 'node:fs';
+import path from 'node:path';
+import sharp from 'sharp';
 
 const wesnothRepoDataDirOnMasterBranch = 'https://github.com/wesnoth/wesnoth/blob/master/data/';
+const transparentColor = { r: 0, g: 0, b: 0, alpha: 0 };
 
 async function blobToBase64(blob: Blob) {
 	const buffer = Buffer.from(await blob.arrayBuffer());
 	return buffer.toString('base64');
 }
+
+const fail500 = () =>
+	fail(500, {
+		error: {
+			description:
+				'Failed to retrieve image from URL. Plase try again. If this error persists, please contact us.'
+		}
+	});
 
 export const actions: Actions = {
 	default: async ({ request }) => {
@@ -13,18 +25,50 @@ export const actions: Actions = {
 		const urlInput = data.get('imageUrl');
 		const { valid, ...validationDetails } = validateUrlInput(urlInput);
 		if (!valid) return fail(400, { error: { description: toErrorDescription(validationDetails) } });
+		const fileName = path.parse(`${urlInput}`).name;
 
 		const rawPng = `${urlInput}?raw=true`;
-		const res = await fetch(rawPng);
-		console.log({ resStatus: res.status });
-		if (res.ok) {
-			console.log('ok');
-			if (res.headers.get('Content-Type') !== 'image/png') console.log('something went wrong');
-			const blob = await res.blob();
-			// await writeFileSync('test.png', new Uint8Array(await blob.arrayBuffer()));
-			return { imageBase64: await blobToBase64(blob) };
+		// const res = await fetch(rawPng);
+		const blob = new Blob([readFileSync('img.png')]);
+		if (true) {
+			const tiny = await sharp(await blob.arrayBuffer()).flop();
+			// const blob = await res.blob();
+			const resizedImg = await sharp(await blob.arrayBuffer())
+				.flop()
+				.resize({
+					width: 80,
+					height: 128,
+					position: sharp.gravity.south,
+					background: transparentColor
+				});
+			writeFileSync('img.png', Buffer.from(await blob.arrayBuffer()));
+			const fullHeroSprite = await resizedImg
+				.clone()
+				.extend({ right: 80 * 4, background: transparentColor })
+				.composite([
+					// overworld image
+					{ top: 40, left: 1 * 80 + 10, input: await tiny.toBuffer() },
+					// overworld in grass
+					{ top: 40, left: 2 * 80 + 10, input: await tiny.toBuffer() },
+					// statue after death
+					{
+						top: 0,
+						left: 3 * 80,
+						input: await resizedImg.clone().tint({ r: 255, g: 215, b: 0, alpha: 1 }).toBuffer()
+					},
+					// ghost after death
+					{
+						top: 0,
+						left: 4 * 80,
+						input: await resizedImg.clone().tint({ b: 255, r: 100, g: 100, alpha: 1 }).toBuffer()
+					}
+				])
+				.toBuffer();
+			return {
+				imageBase64: `data:image/png;base64, ${fullHeroSprite.toString('base64')}`,
+				imageName: fileName
+			};
 		} else {
-			console.log('meh');
 		}
 	}
 };
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,33 +4,94 @@
 			error?: {
 				description: string;
 			};
-			imageBase64: string;
+			imageBase64?: string;
+			imageName?: string;
+			imageUrl?: string;
+			customName?: string;
 		};
 	};
 	let { form }: Props = $props();
-	let image = $derived.by(() => {
-		if (!form?.imageBase64) return '';
-		return `data:image/png;base64, ${form?.imageBase64}`;
-	});
-	$inspect(image);
-	let url = $state('');
+	let url = $state(
+		form?.imageUrl ??
+			'https://github.com/wesnoth/wesnoth/blob/master/data/core/images/units/orcs/leader.png'
+	);
+	let customName = $state(form?.customName ?? '');
+	let copiedEntityDef = $state(false);
+	let usedName = $derived(customName || form?.imageName || '');
+
+	function toEntityDef(name: string) {
+		return `entityDef "wesnoth_${name}_sprite"
+{
+	name "${name}"
+	texturename textures/${name}_sprite
+
+	devname "The Battle for Wesnoth ${form?.imageUrl ?? ''} - Please respect the applicable image licenses. See https://wiki.wesnoth.org/Wesnoth:Copyrights"
+	
+	width 80
+	height 128
+
+	xloc	0
+	yloc	0
+
+	gender none
+}`;
+	}
+
+	async function copyEntityDefToClipboard() {
+		const def = toEntityDef(usedName);
+		await navigator.clipboard.writeText(def);
+		copiedEntityDef = true;
+		setTimeout(() => (copiedEntityDef = false), 3000);
+	}
+
+	function downloadImage() {
+		if (!form?.imageBase64) return;
+		const filename = `${usedName}_sprite.png`;
+		download(form.imageBase64, filename);
+	}
+	function downloadEntityDef() {
+		if (!form?.imageName) return;
+		const filename = `${usedName}.hero`;
+		const dataUrl = `data:text/plain;charset=utf-8,${encodeURIComponent(toEntityDef(usedName))}`;
+		download(dataUrl, filename);
+	}
+	function download(dataUrl: string, filename: string) {
+		const a = document.createElement('a');
+		a.download = filename;
+		a.href = dataUrl;
+		document.body.appendChild(a);
+		a.click();
+		document.body.removeChild(a);
+	}
 </script>
 
 <main>
 	<h1>Dungeonmans Heroskin Maker</h1>
 
 	<form method="POST">
+		<div class="url-label-container">
+			<label for="imageUrl" class="url-label">GitHub URL to Wesnoth unit image</label>
+			<a
+				class="left"
+				href="https://github.com/wesnoth/wesnoth/blob/master/data/core/images/units/"
+				target="_blank"
+				referrerpolicy="no-referrer">Browse Wesnoth core units</a
+			>
+		</div>
+
+		<input
+			bind:value={url}
+			name="imageUrl"
+			id="imageUrl"
+			type="text"
+			placeholder="https://github.com/wesnoth/wesnoth/blob/master/data/core/images/units/orcs/leader.png"
+			required
+		/>
 		<label>
-			Full URL to Wesnoth unit image
-			<input
-				bind:value={url}
-				name="imageUrl"
-				type="text"
-				placeholder="https://github.com/wesnoth/wesnoth/blob/master/data/core/images/units/orcs/leader.png"
-				required
-			/>
+			Custom name (optional)
+			<input bind:value={customName} name="customName" type="text" placeholder="orkleader" />
 		</label>
-		<button type="submit">Convert</button>
+		<div class="submit-button-container"><button type="submit">Make Heroskin</button></div>
 
 		{#if form?.error}
 			<div class="error">
@@ -40,20 +101,97 @@
 		{/if}
 	</form>
 
-	{#if image}
-		<img src={image} alt="loaded img" width="100px" height="100px" />
+	{#if form?.imageBase64 && form?.imageName}
+		<h2 id="results">Your new Heroskin</h2>
+		<div class="img-container">
+			<img src={form.imageBase64} alt="heroskin" />
+			<div class="download-img-btn-container">
+				<button onclick={downloadImage} class="outline download-img-btn">Download</button>
+			</div>
+		</div>
+
+		<div class="entitydef-container">
+			<div class="entitydef-buttons">
+				<button class="outline entitydef-copy-btn" onclick={copyEntityDefToClipboard}
+					>{#if copiedEntityDef}Copied 👍{:else}Copy{/if}</button
+				>
+				<button class="outline entitydef-download-btn" onclick={downloadEntityDef}>Download</button>
+			</div>
+			<textarea readonly>{toEntityDef(usedName)} </textarea>
+		</div>
 	{/if}
+
+	<h3>How to use?</h3>
+	<p>
+		Save or download the heroskin image to
+		<code>
+			c:\users\YOUR_USERNAME\appdata\roaming\Dungeonmans\modcontent\textures\{usedName}.png
+		</code>
+	</p>
+	<p>
+		Download or copy-and-paste the contents of the textfield to
+		<code>
+			c:\users\YOUR_USERNAME\appdata\roaming\Dungeonmans\modcontent\heroskins\{usedName}.hero
+		</code>
+	</p>
+	<p>
+		If you'd like to rename the heroskin, you can provide a 'Custom name' at the top. Be sure to
+		update both image and entity def files though.
+	</p>
 </main>
 
 <style>
 	h1 {
 		text-align: center;
 	}
-	input {
-		word-break: break-all;
-		word-wrap: break-word;
+	button {
+		max-width: 320px;
+	}
+	.submit-button-container {
+		display: flex;
+		justify-content: center;
 	}
 	.error {
 		border: 1px solid red;
+	}
+	textarea {
+		height: 600px;
+	}
+	.url-label-container {
+		display: flex;
+		justify-content: space-between;
+	}
+	.url-label {
+		display: inline;
+	}
+	.entitydef-buttons {
+		position: absolute;
+		top: 0.5rem;
+		right: 0.5rem;
+	}
+	.entitydef-copy-btn {
+		/* position: absolute;
+		top: 0.5rem;
+		right: 0.5rem; */
+		min-width: 180px;
+	}
+	.entitydef-download-btn {
+		/* position: absolute;
+		top: rem;
+		right: 0.5rem; */
+		min-width: 180px;
+	}
+	.entitydef-container {
+		position: relative;
+	}
+	.download-img-btn {
+		min-width: 180px;
+	}
+	.download-img-btn-container {
+		place-content: center;
+	}
+	.img-container {
+		display: flex;
+		justify-content: space-between;
 	}
 </style>
